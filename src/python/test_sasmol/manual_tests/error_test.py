39f85c6d5927a60537af21004e6aeb0a7cb13f39
Joseph E. Curtis <madscatt@gmail.com>
2015-12-22 20:20:09 -0500

    added new error example to manual tests

--- src/python/test_sasmol/manual_tests/error_test.py
+++ src/python/test_sasmol/manual_tests/error_test.py
@@ -1,15 +1,40 @@
-import sasmol.sasmol as sasmol
-#### THIS is to see if we can use our own instance of Error(Exception) ...
-#### not sure this will work cleanly or no
-#### recommended by Google
+import sys
 
-Error = sasmol.Error()
+class log():
 
-#try: 
-#    raise Error
-#except Error as error: 
-#    raise Error('why are you dividing my zero?')
+    def error(self, message):
+        print '\n'+message+'\n'
+        raise 
 
-#finally:
-#    print 'wtf'
+log = log()
 
+try:
+
+    filename = 'data.txt'
+
+    file = open(filename).readlines()
+
+except IOError as error:
+
+    log.error("ERROR: I/O error({0}): {1}".format(error.errno, error.strerror) + " : " + filename)
+
+except:
+
+    log.error("ERROR: Unexpected error:" + str(sys.exc_info()[0]))
+
+
+try:
+
+    for line in file:
+
+        value = int(line.strip())
+
+except ValueError:
+    
+    log.error("ERROR: Could not convert data to an integer: \nline number in file = " + str(file.index(line) + 1) + "\nvalue = " + str(line))
+
+except:
+
+    log.error("ERROR: Unexpected error:" + str(sys.exc_info()[0]))
+    
+    
